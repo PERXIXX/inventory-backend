0bda08816423193690e50abb1c006c773eb81d7c
PERXIXX <131202621+PERXIXX@users.noreply.github.com>
2025-12-08 16:06:13 +0700

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,14 +1,16 @@
 <?php
-$host = getenv("DB_HOST");
-$db   = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
+$host = getenv("PGHOST");
+$db   = getenv("PGDATABASE");
+$user = getenv("PGUSER");
+$pass = getenv("PGPASSWORD");
+$ssl  = getenv("PGSSLMODE") ?: "require";
 
-$dsn = "pgsql:host=$host;dbname=$db;sslmode=require";
+$dsn = "pgsql:host=$host;dbname=$db;sslmode=$ssl";
 
 try {
-    $pdo = new PDO($dsn, $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
 } catch (PDOException $e) {
     die("DB Connection failed: " . $e->getMessage());
 }
