3053a127717dbea5812acbfddcf81210cfabcc80
PERXIXX <131202621+PERXIXX@users.noreply.github.com>
2025-12-08 15:25:34 +0700

    Update login.php

--- api/login.php
+++ api/login.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json; charset=utf-8");
-require_once(__DIR__ . '/config.php');
+require_once(__DIR__ . '/../config.php');
 
 // CORS
 header("Access-Control-Allow-Origin: *");
