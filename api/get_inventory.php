5f475883203005295116523ddc9389258e8f19a5
PERXIXX <131202621+PERXIXX@users.noreply.github.com>
2025-12-08 20:38:20 +0700

    Update get_inventory.php

--- api/get_inventory.php
+++ api/get_inventory.php
@@ -3,8 +3,8 @@ header("Content-Type: application/json; charset=utf-8");
 require_once(__DIR__ . '/config.php');
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     http_response_code(200);
