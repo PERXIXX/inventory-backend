e885a1b79a06b46365f0061f947d306e277be48d
PERXIXX <131202621+PERXIXX@users.noreply.github.com>
2025-12-08 20:38:22 +0700

    Update get_total_sales.php

--- api/get_total_sales.php
+++ api/get_total_sales.php
@@ -1,8 +1,8 @@
 <?php
 header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     http_response_code(200);
