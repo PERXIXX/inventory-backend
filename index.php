136c74bbf61139adbba6c4f1ef9699e77189588d
PERXIXX <131202621+PERXIXX@users.noreply.github.com>
2025-12-08 15:02:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-// Auto route simple API
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-$file = __DIR__ . $path;
-
-if (file_exists($file) && is_file($file)) {
-    include $file;
-    exit;
-}
-
-http_response_code(404);
-echo "API Not Found: " . $path;
+echo json_encode([
+    "status" => "ok",
+    "message" => "PHP Render backend is running!",
+    "api" => "/api/login.php"
+]);
